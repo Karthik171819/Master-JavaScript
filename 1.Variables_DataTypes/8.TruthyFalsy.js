6fbb2c7bbfbd9dee092de0d594f40dfdcb37bdea
Karthik171819 <karthikkumar092004@gmail.com>
2026-02-25 22:49:26 +0530

    tuthy or falsy

---
+++ 1.Variables_DataTypes/8.TruthyFalsy.js
@@ -0,0 +1,7 @@
+//Falsy
+let a = "";
+console.log( a || "ita a Falsy value");
+
+//Truthy
+let b = "Hello";
+console.log( b || "ita a Falsy value"); //hello is a truthy value, so it will be printed instead of the default message
