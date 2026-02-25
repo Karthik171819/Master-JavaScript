c015128f5560c495f84a7d87d18bedf1a6f64e1f
Karthik171819 <karthikkumar092004@gmail.com>
2026-02-25 22:21:57 +0530

    nullish coalescing

---
+++ 1.Variables_DataTypes/6.NullishCoalescing.js
@@ -0,0 +1,5 @@
+let result = null;
+console.log(result ?? "Default Value"); // Output: "Default Value"
+
+result = "Hello, World!";
+console.log(result ?? "Default Value"); // Output: "Hello, World!"
