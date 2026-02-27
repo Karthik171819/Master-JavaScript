40ecba6f11026733ff77d475c9143794867b9afe
Karthik171819 <karthikkumar092004@gmail.com>
2026-02-27 13:24:32 +0530

    Defaukt and Rest params

---
+++ 3.Functions/4.DefaultAndRestParams.js
@@ -0,0 +1,10 @@
+//parameters are placeholders like a, b
+//arguments are actual value like passing the value 2, 4 to the params a, b
+
+//Default parameters
+function greet(name="Guest"){
+    console.log(`Welcome To The AI World ${name}`);
+}
+greet(); //guest
+greet("Karthik") //karthik
+
