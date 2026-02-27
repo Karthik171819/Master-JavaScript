106220d085d434c88489b625ba8c9b7464218a79
Karthik171819 <karthikkumar092004@gmail.com>
2026-02-27 11:45:33 +0530

    functions

--- 3.Functions/1.function.js
+++ 3.Functions/1.function.js
@@ -1,16 +1,19 @@
-function Hello(message){
-    console.log(message);
+function Hello(message) {
+  console.log(message);
 }
 Hello("Hi JS ");
 
-function add(a, b){
-    return a+b;  
+function add(a, b) {
+  return a + b;
 }
 var res = add(2, 3);
 console.log(res);
 
-function sub(a,b){
-    return a-b;
+function sub(a, b) {
+  if (a && b) {
+    return a - b;
+  }
+  return 0;
 }
-var res = sub(10, 6);
+var res = sub();
 console.log(res);
