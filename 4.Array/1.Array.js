add21e76e878c6a427407f0782836793809c0464
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-01 22:57:58 +0530

    Array

--- 4.Array/1.Array.js
+++ 4.Array/1.Array.js
@@ -2,6 +2,6 @@
 let colors = ['red', 'blue', 'white'];
 console.log(colors);
 
-//we can create an array using in built Array method
+//we can create an array using in built Array class method
 let colorss = new Array("blue", "pink", "pale");
 console.log(colorss);
