21fe1f0733d940db8f928c61c28ad068b62a0b96
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-01 22:55:02 +0530

    Array

---
+++ 4.Array/1.Array.js
@@ -0,0 +1,7 @@
+//array is used to store several types of datatypes
+let colors = ['red', 'blue', 'white'];
+console.log(colors);
+
+//we can create an array using in built Array method
+let colorss = new Array("blue", "pink", "pale");
+console.log(colorss);
