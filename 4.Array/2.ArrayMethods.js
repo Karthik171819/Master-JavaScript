960ec0505a9b4bfaf3ef097d0b452a0a89e43614
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-02 21:17:37 +0530

    Array method

---
+++ 4.Array/2.ArrayMethods.js
@@ -0,0 +1,15 @@
+let player = ["Abd", "Virat", "Dhoni", "Sachin"];
+player.push("Surya"); //used to push the value at the end of an array.
+
+//to insert a value at initial position 
+player.unshift("surya")
+
+//to remove the end of an element in array
+player.pop(); //surya poped
+
+//to rem ove the first value of an array
+player.shift(); //surya removed
+
+//to find out the element index position in array by giving the certain exact value
+player.indexOf('Abd');
+console.log(player)
