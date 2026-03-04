931bb009fd11e826ff22a77d560f41308999a4d3
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-04 13:23:05 +0530

    Array mapping method

---
+++ 4.Array/4.ArrayMap.js
@@ -0,0 +1,14 @@
+//map() is an array method used to transform each element of an array and return a new array.
+// It does not change the original array.
+//syntax
+// array.map((element, index, array) => {
+//     return newValue;
+// });
+const products = ["beans", "brinjal", "tomato", "potato"];
+const mappedProducts = products.map((el) => { return el+' $'});
+console.log(mappedProducts);
+
+//example
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map((num) => {return num*2});
+console.log(doubled);
