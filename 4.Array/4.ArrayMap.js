e94effd6213eb45b94550e93796a642d98696e06
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-04 15:15:34 +0530

    Array mapping method

--- 4.Array/4.ArrayMap.js
+++ 4.Array/4.ArrayMap.js
@@ -12,3 +12,9 @@ console.log(mappedProducts);
 const numbers = [1, 2, 3, 4];
 const doubled = numbers.map((num) => {return num*2});
 console.log(doubled);
+
+//discount prices
+const price = [2, 3, 4, 1, 5, 90];
+const dprice = 1;
+const result = price.map((amt) => amt - dprice );
+console.log(result);
