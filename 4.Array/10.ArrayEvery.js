62c02c32004be565fa6f2e6678c65bc92f0a2c8b
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-07 13:11:31 +0530

    Array Every

---
+++ 4.Array/10.ArrayEvery.js
@@ -0,0 +1,7 @@
+// every() method is used to check if all elements in an array satisfy a condition. It returns true if every element passes the test, otherwise false.
+const n = [20, 30, 40, 45];
+console.log(n.every(n => n>15));
+
+const n1 = [ 14, 20, 30, 40, 45];
+console.log(n1.every(n => n>15)); //false
+
