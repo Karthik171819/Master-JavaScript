137523a4c2e9aa5830367735ec4462d53666eb8e
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-07 13:19:58 +0530

    Array includes method

---
+++ 4.Array/11.ArrayIncludes.js
@@ -0,0 +1,8 @@
+// use includes when you are comparing the strings of an array you can find the particular element also
+const num = ["Apple", "Banana", "Orange"];
+if(num.includes("Banana")){
+    console.log("yes");
+} else {
+    console.log("No");
+ 
+}
