67f8fafe2210b35bd7dfcfe46b57dd6917d8a8c2
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-16 23:30:10 +0530

    Student score project

--- Project1/StudentScore.js
+++ Project1/StudentScore.js
@@ -4,8 +4,14 @@ function addStudents(name, mark){
     mark = Number(mark);
     mark = mark ?? 0;
     name = name.trim().toUpperCase();
-    students.push({name, score})
+    students.push({name, mark})
 }
+
+//adding students data to console log
+addStudents("Virat", "99");
+addStudents("Dhoni", 82);
+addStudents("Anisha", 44);
+addStudents("Sana", 29);
 
 //Get Grades
 function getGrades(){
@@ -22,6 +28,41 @@ function getGrades(){
 
 //Top Scorers
 function getTopScores(){
-    students.filter((student) => student.score)
-
+      return students.filter((student) => student.mark >= 75)
 }
+
+//find student
+function findStudent(name){
+   return students.find((student) => student.name == name)
+}
+
+// check failures
+function hasFailures() {
+    return students.some((student) => student.mark < 35)
+}
+
+//display data
+function displayAll(){
+     const graded = getGrades();
+     console.log("Student Data");
+     console.log("All Student with Grades");
+     console.log("NAME | MARK | GRADE")
+     graded.forEach((student) => {
+        console.log(`${student.name} | ${student.mark} | ${student.grade}`)
+     })
+}
+
+displayAll();
+
+//Top Scorer List
+console.log("\n TopScores:");
+console.log(getTopScores());
+
+//Find student
+console.log("\n searching for student")
+console.log(findStudent("Virat"));
+
+//check Fail Students
+console.log("\n Any Failures")
+console.log(hasFailures() ? "Yes Some student failed" : "No failed students")
+
