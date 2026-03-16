a09b65c11469e5bb4489be8ee23e365eb393fe93
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-16 23:40:23 +0530

    Objects

---
+++ 5.Objects/1.Object.js
@@ -0,0 +1,10 @@
+// In JavaScript, an object is a data type used to store key–value pairs. It helps group related data and functions together.
+// An object is used to store multiple related values in one variable. in simple terms
+//firstName and all not a variables its a property or fields because its inside an object so we should call like that only
+let person ={
+    firstName: "Karhtik",
+    lastName: "K",
+    role: "Associate SDU",
+    isEmployee: true,
+};
+console.log(person.role);
