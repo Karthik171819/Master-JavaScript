7d6965e05076d94fc8915affc67b711206ceb5a7
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-17 14:13:57 +0530

    Objects keys

---
+++ 5.Objects/3.ObjectsKeys.js
@@ -0,0 +1,9 @@
+//object keys method used to get the only keys in object
+const student = {
+    name: "john",
+    class: 10,
+    result: "Pass",
+    merit: "distinction"
+};
+
+console.log(Object.keys(student));
