d9e42657e4e5a04539d83b7fd99dea8263396a2c
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-18 12:46:45 +0530

    Objects Entries

---
+++ 5.Objects/4.ObjectsEntries.js
@@ -0,0 +1,15 @@
+const obj = {
+    name: "lok",
+    age: 40,
+    loc: "NYC"
+}
+console.log(Object.entries(obj));
+
+//another use of entries it will arrange the integer key from asec to desc
+const num = {
+    12: "hi",
+    8: "hy",
+    1:"kk",
+    100: "jnjnd"
+}
+console.log(Object.entries(num));
