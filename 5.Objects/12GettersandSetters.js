f8624da5fb5ff3abb10106cb6d869f1b9df72bca
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-28 18:05:51 +0530

    getters and setter

--- 5.Objects/12GettersandSetters.js
+++ 5.Objects/12GettersandSetters.js
@@ -1,10 +1,13 @@
 const person ={
     fname: "Karthik",
     lname: "k",
+
+    //getter
     get fullName(){
         return `${this.fname} ${this.lname}`;
     },
 
+    //setter
     set fullName(value){
         const [first, last] = value.split(" ");
         this.firstName = first;
