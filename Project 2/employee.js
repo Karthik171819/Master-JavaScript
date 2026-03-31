e72034109f37ed9c63a70eff3730c37ad700b6b5
Karthik171819 <karthikkumar092004@gmail.com>
2026-03-31 15:53:16 +0530

    Employee simple project

--- Project 2/employee.js
+++ Project 2/employee.js
@@ -1,25 +1,35 @@
-function Employees(id, name, department, salary){
-    this.id = id;
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+function Employees(id, name, department, salary) {
+  this.id = id;
+  this.name = name;
+  this.department = department;
+  this.salary = salary;
 }
 
 //create a simple employee database object
 const employeeDB = {};
 
 //function to add employee to the database
-function addEmployee(id, name, department, salary){
-    const newEmployee = new Employees(id, name, department, salary);
-    employeeDB[id] = newEmployee;
-    console.log(`Employee  with ID${id} added successfully.`);
+function addEmployee(id, name, department, salary) {
+  const newEmployee = new Employees(id, name, department, salary);
+  employeeDB[id] = newEmployee;
+  console.log(`Employee  with ID${id} added successfully.`);
 }
 
 //function to get all employees object.values
-function getAllEmployees(){
-   const employees =  Object.values(employeeDB);
-   console.log("List of All Employees:");
-   employees.forEach(employee => {
-    console.log(`ID: ${employee.id}, Name: ${employee.name}, Department: ${employee.department}, Salary: ${employee.salary}`);
-   });
+function getAllEmployees() {
+  const employees = Object.values(employeeDB);
+  console.log("List of All Employees:");
+  employees.forEach((employee) => {
+    console.log(
+      `ID: ${employee.id}, Name: ${employee.name}, Department: ${employee.department}, Salary: ${employee.salary}`,
+    );
+  });
+}
+
+//Function to get employee details by ID Object.entries
+function getEmployeeDetails(id) {
+  if (employeeDB[id]) {
+    return Object.entries(employeeDB[id]);
+  }
+  return `Employee with ID ${id} not found.`;
 }
