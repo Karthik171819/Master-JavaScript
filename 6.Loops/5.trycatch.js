cae8e3eb221272eb5c9e39095c6ed557f06a6775
Karthik171819 <karthikkumar092004@gmail.com>
2026-04-14 19:08:44 +0530

    try catch

--- 6.Loops/5.trycatch.js
+++ 6.Loops/5.trycatch.js
@@ -1,4 +1,16 @@
 try{
+const a = ad(5, 10);
+console.log(a)
+} catch(e){
+console.log({name: e.name, message: e.message})
+}
+console.log("This will run even if there is an error in the above code")
+
+//without error object
+function add(a, b){
+    return a + b;
+}
+try{
 const a = add(5, 10);
 console.log(a)
 } catch(e){
