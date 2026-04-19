c0565d6e42b4efee4c7845f1a7a3264617844bda
Karthik171819 <karthikkumar092004@gmail.com>
2026-04-19 11:20:08 +0530

    Promise

---
+++ 6.Loops/8.Promise.js
@@ -0,0 +1,11 @@
+function getUser() {
+  users = [
+    { name: "Karthik", loc: "banglore" },
+    { name: "bob", loc: "chennai" },
+  ];
+
+//   return users;
+  
+  console.log(users);
+}
+getUser();
