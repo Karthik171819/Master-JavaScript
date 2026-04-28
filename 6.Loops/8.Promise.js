36623fb6b6aa332496901e80a3e9d1d789454721
Karthik171819 <karthikkumar092004@gmail.com>
2026-04-28 20:14:40 +0530

    Promise

--- 6.Loops/8.Promise.js
+++ 6.Loops/8.Promise.js
@@ -1,11 +1,18 @@
 function getUser() {
-  users = [
+return new Promise((res, rej) => {
+  let users=[];
+  setTimeout(() => {
+      users = [
     { name: "Karthik", loc: "banglore" },
     { name: "bob", loc: "chennai" },
   ];
+  res(users)
+  }, 2000);
+   return users;
+})
 
-//   return users;
-  
-  console.log(users);
 }
-getUser();
+let promise = getUser()
+promise.then((users) => {
+  console.log(users)
+})
