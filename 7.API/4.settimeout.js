705e9c34fc4ee734231c03be880741bf613e999e
Karthik171819 <karthikkumar092004@gmail.com>
2026-05-10 21:23:41 +0530

    set timeout

--- 7.API/4.settimeout.js
+++ 7.API/4.settimeout.js
@@ -2,3 +2,7 @@ const timer = setTimeout(() => {
     console.log('Hello, World! after 3sec im execute please wait');
 }, 3000);
 console.log(timer) //its just for your reference
+
+setInterval(( ) => {
+    console.log("Hello, World! im execute every 2sec")
+}, 2000)
