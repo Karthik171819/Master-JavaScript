46514165e88d1612cc259fdbbc8c6bbb32da3215
Karthik171819 <karthikkumar092004@gmail.com>
2026-05-10 21:33:25 +0530

    local storage

---
+++ 7.API/5.localstorage.js
@@ -0,0 +1,9 @@
+localStorage.setItem('name', 'Vikram');
+localStorage.setItem('age', 30);
+
+localStorage.getItem('name'); // "Vikram"
+localStorage.getItem('age'); // "30"
+
+
+//this code only works on browser console coz by default nodejs dont have localstorage.
+
