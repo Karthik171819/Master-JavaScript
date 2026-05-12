42bcf9b3f9b76ec730369d78bdd59e8218dcd622
Karthik171819 <karthikkumar092004@gmail.com>
2026-05-12 20:02:00 +0530

    ESmodule import/export

--- 7.API/6.ESmain.js
+++ 7.API/6.ESmain.js
@@ -1,3 +1,7 @@
 //importing through ESmodules
+//benefits of ESmodules
+//Modularity - ES modules allow you to break your code into smaller, reusable pieces. This promotes better organization and maintainability of your codebase.
+//Resuability - You can export functions, objects, or values from one module and import them into another module. This promotes code reuse and reduces duplication.
+//maintainability - By using ES modules, you can keep your code organized and easier to maintain. Each module can have its own scope, which helps prevent naming conflicts and makes it easier to understand the structure of your code.
 import {add} from './6.ESmodules.js';
 console.log(add(2, 3));
